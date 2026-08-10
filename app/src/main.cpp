c14336c72799fe05106a078325f6f9a4305f68f4
fshstk <fshstk@fshstk.com>
2026-08-10 13:29:50 +0000

    [l1-task1] add fourth cooperative thread w/ prio -1
    Observer behavior:
    
    As before, but now T_COOP runs through all five iterations at the start,
    before any of the other threads run, and even before `main()` starts.
    
    Even logging does not show up (unless we add
    `CONFIG_LOG_MODE_IMMEDIATE=y`) until the thread has finished.
    
    `k_yield()` is redundant since the thread implicitly yields when it
    exits.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -28,15 +28,26 @@ void t_high_fn() {
   }
 }
 
-const auto STACK_SIZE = 1024;
-const auto PRIO_LOW = 7;
-const auto PRIO_MED = 5;
-const auto PRIO_HIGH = 3;
+void t_coop_fn() {
+  constexpr auto num_loops = 5;
+  for (auto count = 1U; count <= num_loops; ++count) {
+    LOG_INF("T_COOP running\t[count %d/%d]", count, num_loops);
+    k_busy_wait(500'000);
+  }
+  k_yield();  // technically redundant since function exits...
+}
+
+const size_t STACK_SIZE = 1024;
+const int PRIO_LOW = 7;
+const int PRIO_MED = 5;
+const int PRIO_HIGH = 3;
+const int PRIO_COOP = -1;
 
 // clang-format off
 K_THREAD_DEFINE(t_low,  STACK_SIZE, t_low_fn,  nullptr, nullptr, nullptr, PRIO_LOW,  0, 0);
 K_THREAD_DEFINE(t_med,  STACK_SIZE, t_med_fn,  nullptr, nullptr, nullptr, PRIO_MED,  0, 0);
 K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn, nullptr, nullptr, nullptr, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn, nullptr, nullptr, nullptr, PRIO_COOP, 0, 0);
 // clang-format on
 }  // namespace
 
