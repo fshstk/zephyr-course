c8e06f66baf6864394dd7d8afac3163cc8cd242c
fshstk <fshstk@fshstk.com>
2026-08-19 19:46:08 +0200

    add "bad" example of shared state w/o thread sync
    Unfortunately I couldn't reproduce the race condition.
    
    During Q&A it was mentioned that modern embedded CPUs are often so fast
    that the error does not appear with simple examples. Slow operations
    like peripheral access are much more dangerous.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,57 +1,40 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include "fsh/Semaphore.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 namespace {
-void t_low_fn() {
-  size_t count = 0;
-  while (true) {
-    LOG_INF("T_LOW running\t[count %d]", ++count);
-    k_msleep(300);
-  }
-}
+size_t count = 0;
+fsh::Semaphore count_done;
+const size_t MAX_COUNT = 100'000;
 
-void t_med_fn() {
-  size_t count = 0;
-  while (true) {
-    LOG_INF("T_MED running\t[count %d]", ++count);
-    k_msleep(200);
-  }
-}
-
-void t_high_fn() {
-  size_t count = 0;
-  while (true) {
-    LOG_INF("T_HIGH running\t[count %d]", ++count);
-    k_msleep(100);
-  }
-}
-
-void t_coop_fn() {
-  constexpr auto num_loops = 5;
-  for (auto count = 1U; count <= num_loops; ++count) {
-    LOG_INF("T_COOP running\t[count %d/%d]", count, num_loops);
-    k_busy_wait(500'000);
-  }
-  k_yield();  // technically redundant since function exits...
+void increment() {
+  for (size_t i = 0; i < MAX_COUNT; ++i)
+    count++;
+  count_done.give();
+  const auto* thread_name = k_thread_name_get(k_current_get());
+  LOG_INF("[%s]: increment finished", thread_name);
 }
 
 const size_t STACK_SIZE = 1024;
-const int PRIO_LOW = 7;
 const int PRIO_MED = 5;
-const int PRIO_HIGH = 3;
-const int PRIO_COOP = -1;
 
 // clang-format off
-K_THREAD_DEFINE(t_low,  STACK_SIZE, t_low_fn,  nullptr, nullptr, nullptr, PRIO_LOW,  0, 0);
-K_THREAD_DEFINE(t_med,  STACK_SIZE, t_med_fn,  nullptr, nullptr, nullptr, PRIO_MED,  0, 0);
-K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn, nullptr, nullptr, nullptr, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn, nullptr, nullptr, nullptr, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(increment_a, STACK_SIZE, increment, nullptr, nullptr, nullptr, PRIO_MED, 0, 0);
+K_THREAD_DEFINE(increment_b, STACK_SIZE, increment, nullptr, nullptr, nullptr, PRIO_MED, 0, 0);
 // clang-format on
 }  // namespace
 
 int main() {
+  auto t_start = k_uptime_get();
   LOG_INF("main() entered");
+  count_done.take();
+  count_done.take();
+  const auto t_delta = k_uptime_delta(&t_start);
+  const auto expected = 2 * MAX_COUNT;
+  LOG_INF("count done: %d (expected %d)\t[%lld ms]", count, expected, t_delta);
+  if (count != expected)
+    LOG_ERR("expected and actual values do not match");
   return 0;
 }
